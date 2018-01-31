a43ff45bf9427bc6bd1e63aad471b5ede3de5d43
kulishetl <kulishetl@gmail.com>
2018-01-31 16:09:59 +0200

    total redo module-07

--- advance/module-07/js/script.js
+++ advance/module-07/js/script.js
@@ -1,63 +1,75 @@
-function createLayout(lang) {
-    //создаем строки
-    const topRow = `
-                <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                </ul>
-                `,
-        middleRow = `
-                <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                </ul>
-                `,
-        bottomRow = `
-                <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                </ul>
-                `,
-        keyboard = topRow + middleRow + bottomRow,
-        //выбор места вставки
-        keys = document.querySelector("body");
-        //вставка в DOM объект
-    keys.insertAdjacentHTML('beforeend', keyboard);
-    
-    const elements = document.getElementsByTagName('li');
-    let arr = [...elements];
-    //простановка значений на клавиши
-    return arr.map((elem, i) => elem.textContent = lang[i]);
-}
-
-let strEn = "qwertyuiop[]asdfghjkl;'zxcvbnm,./";
-//let strRu = "йцукенгшщзхъфывапролджэячсмитьбю.";
-//let strUa = "йцукенгшщзхїфівапролджєячсмитьбю.";
-createLayout(strEn);
+const keyboard = {
+    layouts: {
+      en: {
+        topRow: ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]"],
+        middleRow: ["a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'"],
+        bottomRow: ["z", "x", "c", "v", "b", "n", "m", ",", ".", "/"]
+      },
+      ru: {
+        topRow: ["й", "ц", "у", "к", "е", "н", "г", "ш", "щ", "з", "х", "ъ"],
+        middleRow: ["ф", "ы", "в", "а", "п", "р", "о", "л", "д", "ж", "э"],
+        bottomRow: ["я", "ч", "с", "м", "и", "т", "ь", "б", "ю", "."]
+      },
+      ua: {
+        topRow: ["й", "ц", "у", "к", "е", "н", "г", "ш", "щ", "з", "х", "ї"],
+        middleRow: ["ф", "і", "в", "а", "п", "р", "о", "л", "д", "ж", "є"],
+        bottomRow: ["я", "ч", "с", "м", "и", "т", "ь", "б", "ю", "."]
+      }
+    },
+    langs: ["en", "ru", "ua"],
+    currentLang: '',
+    createLayout() {
+//создание обертки для клавиатуры
+      let wrap = document.createElement('div');
+//создание списков для строк      
+      let topRowEl = document.createElement('ul');
+      topRowEl.classList.add('top');
+      let middleRowEl = document.createElement('ul');
+      middleRowEl.classList.add('middle');
+      let bottomRowEl = document.createElement('ul');
+      bottomRowEl.classList.add('bottom');
+//создание элементов списка
+      let topRowItems = this.layouts[this.langs[this.currentLang]].topRow.map(n => n = '<li></li>').reduce((acc, next) => acc + next);
+      let middleRowItems = this.layouts[this.langs[this.currentLang]].middleRow.map(n => n = '<li></li>').reduce((acc, next) => acc + next);
+      let bottomRowItems = this.layouts[this.langs[this.currentLang]].bottomRow.map(n => n = '<li></li>').reduce((acc, next) => acc + next);
+//выбор места для вставки клавиатуры
+      const keys = document.querySelector("body");
+//сборка элементов
+      wrap.append(topRowEl, middleRowEl, bottomRowEl);
+      topRowEl.insertAdjacentHTML('beforeend', topRowItems);
+      middleRowEl.insertAdjacentHTML('beforeend', middleRowItems);
+      bottomRowEl.insertAdjacentHTML('beforeend', bottomRowItems);
+      keys.append(wrap);
+//формирование массивов из элементов списка по строкам
+      const elTop = topRowEl.querySelectorAll('li'),
+            elMiddle = middleRowEl.querySelectorAll('li'),
+            elBottom = bottomRowEl.querySelectorAll('li');
+      let arrTop = [...elTop],
+          arrMiddle = [...elMiddle],
+          arrBottom = [...elBottom];
+//запись букв текущего языка в кнопки клавиатуры
+      return arrTop.map((el, i) => el.textContent = this.layouts[this.langs[this.currentLang]].topRow[i])+
+             arrMiddle.map((el, i) => el.textContent = this.layouts[this.langs[this.currentLang]].middleRow[i])+
+             arrBottom.map((el, i) => el.textContent = this.layouts[this.langs[this.currentLang]].bottomRow[i]);
+    },
+//выбор языка клавиатуры
+    setCurrentLang () {
+      return prompt("Выберите язык клавиатуры: en-0, ru-1, ua-2", '');
+    },
+//проверка корректности выбора языка
+    checkPositiveInteger() {
+      let d, langChoise;
+      do {
+        d = this.setCurrentLang();
+        langChoise = (d === "0") || (d === "1") || (d === "2");
+      } while (!langChoise);
+      this.currentLang = +d;
+      return this;
+      }
+  };
+  
+  function run() {
+    keyboard.checkPositiveInteger().createLayout();
+  }
+  
+  run();
