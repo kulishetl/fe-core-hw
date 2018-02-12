c98fa5c566dcb6afa14e89773e0f055375609c6e
kulishetl <kulishetl@gmail.com>
2018-02-12 11:19:52 +0200

    redo gui of module-11

--- advance/module-11/js/script.js
+++ advance/module-11/js/script.js
@@ -6,37 +6,37 @@
 функция updateUser должна обновлять данные пользователя по id.
 Сделать минимальный графический интерфейс в виде панели с полями и кнопками, а так же панелью для вывода результатов операций с REST сервисом.
 */
-
+//ссылка на rest сервис
 const urlRest = "http://fecore.net.ua/rest/";
-
 //Элементы блока получения данных
 const getBtn = document.getElementById("js-get");
-const getRes = document.querySelector(".get-result");
 
 //Элементы блока добавления новой записи
 const addName = document.getElementById("add-name");
 const addScore = document.getElementById("add-score");
 const addBtn = document.getElementById("js-add");
-const addRes = document.querySelector(".add-result");
 
 //Элементы блока удаления записи
 const removeId = document.getElementById("remove-id");
 const removeBtn = document.getElementById("js-remove");
-const removeRes = document.querySelector(".remove-result");
+
 
 //Элементы блока обновления информации
 const updateId = document.getElementById("update-id");
 const updateName = document.getElementById("update-name");
 const updateScore = document.getElementById("update-score");
 const updateBtn = document.getElementById("js-update");
-const updateRes = document.querySelector(".update-result");
 
+//поле записи результатов работы
+const result = document.querySelector(".result");
 
 getBtn.addEventListener("click", getUsers);
 addBtn.addEventListener("click", addUser);
 removeBtn.addEventListener("click", removeUser);
 updateBtn.addEventListener("click", updateUser);
 
+
+//получение списка пользователей
 function getUsers (evt) {
     evt.preventDefault();
     fetch(urlRest, {method: "post"})
@@ -55,8 +55,9 @@ function getUsers (evt) {
     })
 };
 
+
 const renderData = (data) => {
-    getRes.innerHTML = `<ul>
+    result.innerHTML = `<ul>
                            <li>Id</li>
                            <li>Name</li>
                            <li>Score</li>
@@ -66,71 +67,89 @@ const renderData = (data) => {
                                        <li>${el.name}</li>
                                        <li>${el.score}</li>
                                    </ul>`).reduce((accum, elem) => accum += elem);
-                                   getRes.innerHTML += res;
+                                   result.innerHTML += res;
    };
 
+//добавление новой записи
 function addUser (evt) {
     evt.preventDefault();
-    const addUrl = `${urlRest}?action=1&name=${addName.value}&score=${addScore.value}`;
-    fetch(addUrl, {method: "post"})
-    .then(response => {
-        if(response.ok) {
-            return response.json();
-        } else {
-            return  new Error("Error fetching data");
-        }
-    })
-    .then(data => {
-        addRes.innerHTML = `Создание пользователя, ответ сервера:  ${data[0].mysql}`
-    })
-    .catch(err => {
-        console.error("Error: ", err);
-        addRes.innerHTML = `Error`;
-      })
-      addName.value = "";
-      addScore.value = "";
+    if(addName.value && addScore.value) {
+        const addUrl = `${urlRest}?action=1&name=${addName.value}&score=${addScore.value}`;
+        fetch(addUrl, {method: "post"})
+        .then(response => {
+            if(response.ok) {
+                return response.json();
+            } else {
+                return  new Error("Error fetching data");
+            }
+        })
+        .then(data => {
+            result.innerHTML = `Создание пользователя, ответ сервера:  ${data[0].mysql}`
+        })
+        .catch(err => {
+            console.error("Error: ", err);
+            result.innerHTML = `Error`;
+          })
+          addName.value = "";
+          addScore.value = "";
+    } else {
+        result.innerHTML = `Необходимо заполнить все поля формы "ADD USER"`;
+    }
+    
 };
 
+//удаление записи
 function removeUser (evt) {
     evt.preventDefault();
-    const removeUrl = `${urlRest}?action=3&id=${removeId.value}`;
-    fetch(removeUrl, {method: "post"})
-    .then(response => {
-        if(response.ok) {
-            return response.json();
-        } else {
-            return  new Error("Error fetching data");
-        }
-    })
-    .then(data => {
-        removeRes.innerHTML = `Удаление пользователя ответ сервера:  ${data[0].mysql}`
-    })
-    .catch(err => {
-        console.error("Error: ", err);
-        removeRes.innerHTML = `Error`;
-      })
-      removeId.value = "";
+    if(removeId.value) {
+        const removeUrl = `${urlRest}?action=3&id=${removeId.value}`;
+        fetch(removeUrl, {method: "post"})
+        .then(response => {
+            if(response.ok) {
+                return response.json();
+            } else {
+                return  new Error("Error fetching data");
+            }
+        })
+        .then(data => {
+            result.innerHTML = `Удаление пользователя ответ сервера:  ${data[0].mysql}`
+        })
+        .catch(err => {
+            console.error("Error: ", err);
+            result.innerHTML = `Error`;
+          })
+          removeId.value = "";
+    } else {
+        result.innerHTML = `Необходимо заполнить все поля формы "REMOVE USER"`;
+    }
+    
 };
 
+//обновление данных записи
 function updateUser (evt) {
     evt.preventDefault();
-    const updateUrl = `${urlRest}?action=2&id=${updateId.value}&name=${updateName.value}&score=${updateScore.value}`;
-    fetch(updateUrl, {method: "post"})
-    .then(response => {
-        if(response.ok) {
-            return response.json();
-        } else {
-            return  new Error("Error fetching data");
-        }
-    })
-    .then(data => {
-        updateRes.innerHTML = `Обновление данных пользователя, ответ сервера:  ${data[0].mysql}`
-    })
-    .catch(err => {
-        console.error("Error: ", err);
-        updateRes.innerHTML = `Error`;
-      })
-      updateId.value = "";
-      updateName.value = "";
-      updateScore.value = "";
+    if(updateId.value && updateName.value && updateScore.value) {
+        const updateUrl = `${urlRest}?action=2&id=${updateId.value}&name=${updateName.value}&score=${updateScore.value}`;
+        fetch(updateUrl, {method: "post"})
+        .then(response => {
+            if(response.ok) {
+                return response.json();
+            } else {
+                return  new Error("Error fetching data");
+            }
+        })
+        .then(data => {
+            result.innerHTML = `Обновление данных пользователя, ответ сервера:  ${data[0].mysql}`
+        })
+        .catch(err => {
+            console.error("Error: ", err);
+            result.innerHTML = `Error`;
+          })
+          updateId.value = "";
+          updateName.value = "";
+          updateScore.value = "";
+    } else {
+        result.innerHTML = `Необходимо заполнить все поля формы "UPDATE USER"`;
+    }
+    
 };
